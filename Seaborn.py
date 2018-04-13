5d6d4ec7e5d41a834e5dbf95da9fc1c65a68d05e
Claire Drummond <g00364781@gmit.ie>
2018-04-13 11:55:50 +0100

    Added Seaborn.py

---
+++ Seaborn.py
@@ -0,0 +1,14 @@
+# Claire Drummond April-13-2018
+# https://stackoverflow.com/questions/45862223/use-different-colors-in-scatterplot-for-iris-dataset
+# Scatterplot that shows the ratio between the width & length of Sepal Leafs & Petal Leafs
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+iris = sns.load_dataset("iris")
+iris["ID"] = iris.index
+iris["ratio"] = iris["sepal_length"]/iris["sepal_width"]
+
+sns.lmplot(x="ID", y="ratio", data=iris, hue="species", fit_reg=False, legend=False)
+
+plt.legend()
+plt.show()
